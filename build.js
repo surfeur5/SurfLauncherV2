bc42ff7ea23042529ecf423c2f475586aabf5eec
BuildTools <unconfigured@null.spigotmc.org>
2021-02-04 13:19:57 +0100

    edit config

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'Surflauncher',
+        productName: 'Surfruncraft Launcher Dev',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2018-2021',
         directories: {
             buildResources: 'build',
             output: 'dist'
